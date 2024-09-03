88e2c089cc4f82f49d0f23b667d2e5fc6329509c
Adamya singh <145559694+adamyasingh22@users.noreply.github.com>
2024-09-03 15:05:34 +0000

    monotonic array

---
+++ Leetcode Array/896.js
@@ -0,0 +1,21 @@
+// monotonic array
+
+var isMonotonic = function(nums) {
+    let increasing = true;
+    let decreasing = true;
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]< nums[i+1]){
+            decreasing = false;
+        }
+        else if(nums[i]> nums[i+1]){
+            increasing = false;
+        }
+
+        if(!increasing && !decreasing){
+            return false;
+        }
+
+    }
+    return true;
+};
