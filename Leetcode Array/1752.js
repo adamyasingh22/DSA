d906d20c290be7169e95fb4daadc5bc62fa89872
Adamya singh <145559694+adamyasingh22@users.noreply.github.com>
2024-09-04 12:49:53 +0000

    array is sorted and rotated

---
+++ Leetcode Array/1752.js
@@ -0,0 +1,13 @@
+//Check if the array is sorted
+
+var check = function(nums) {
+   let order=0
+
+   for(let i=0;i<nums.length;i++){
+    if(nums[i]>nums[i+1]) order++
+   }
+
+    nums[0]<nums[nums.length-1] && order++
+
+    return order<2  
+};
